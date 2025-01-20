aadba14aa9c620fddf6ed3c8e31396c72321d325
DyLaNHurtado <dylanhurtado43@gmail.com>
2025-01-20 19:16:14 +0100

    Agregar vista NotFound y configurar ruta para manejar páginas no encontradas

--- frontend/src/routes/index.ts
+++ frontend/src/routes/index.ts
@@ -3,6 +3,7 @@ import MainLayout from '@/layouts/MainLayout.vue';
 import HomeView from '@/views/HomeView.vue';
 import DetailsView from '@/views/DetailsView.vue';
 import FavoritesView from '@/views/FavoritesView.vue';
+import NotFound from '@/views/NotFound.vue';
 
 const routes = [
   {
@@ -12,7 +13,19 @@ const routes = [
       { path: '', name: 'home', component: HomeView },
       { path: 'ad/:id', name: 'details', component: DetailsView, props: true },
       { path: 'favorites', name: 'favorites', component: FavoritesView },
+      // { path: 'contact', name: 'contact', component: ContactView },
+      // { path: 'about', name: 'about', component: AboutView },
+      // { path: 'profile', name: 'profile', component: ProfileView },
+      // { path: 'my-ads', name: 'my-ads', component: MyAdsView },
+      // { path: 'register', name: 'register', component: RegisterView },
+      // { path: 'login', name: 'login', component: LoginView },
+      // { path: 'advanced-search', name: 'advanced-search', component: AdvancedSearchView },
     ],
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'not-found',
+    component: NotFound,
   },
 ];
 
---
+++ frontend/src/views/NotFound.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-800">
+      <div class="flex flex-col items-center">
+        <svg class="h-32 w-32 text-gray-600 mb-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <path fill="currentColor" d="M12 2L2 12h3v8h6v-6h2v6h6v-8h3L12 2z"></path>
+        </svg>
+        <h1 class="text-4xl font-bold mb-4">Página no encontrada</h1>
+        <p class="text-lg mb-8">Lo sentimos, no pudimos encontrar la página que estás buscando.</p>
+        <img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExYW5zNjB1ZHNldXpseHV5cnkyZnF1c20yNHRuZWxwaXlqcjZraGoyMSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/geDYq73orMiMLQ1bH5/giphy.gif" alt="Lost House" class="mb-8 w-64 h-64 object-cover rounded-lg shadow-lg">
+        <p class="text-lg mb-8">Parece que esta casa se ha perdido también...</p>
+        <RouterLink to="/" class="text-primary hover:underline text-lg">Volver a la página principal</RouterLink>
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { RouterLink } from 'vue-router';
+  </script>
+  
+  <style scoped>
+  .text-primary {
+    color: #DBBB31;
+  }
+  </style>
